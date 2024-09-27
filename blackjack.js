d907c0a7ca4e153c1b10b19f8673b5d6398b76ea
jeminkim <kjm972@gmail.com>
2024-09-27 15:35:49 +0900

    test

---
+++ blackjack.js
@@ -0,0 +1,29 @@
+let playerCards = [10, 7];
+let playerSum = playerCards.reduce((a, b) => a + b, 0);
+
+let dealerCards = [8, 5];
+let dealerSum = dealerCards.reduce((a, b) => a + b, 0);
+
+if (playerSum === 21) {
+    console.log('You Win')
+} else if (playerSum > 21) {
+    console.log('You lost dealer Win')
+} else {
+    while (dealerSum < 17) {
+        let newCard = Math.floor(Math.random() * 11) + 1;
+        dealerSum += newCard
+    }
+} 
+if (dealerSum > 21) {
+    console.log(`dealer ${dealerSum} bust You Win`)
+} else if (playerSum === dealerSum) {
+    console.log('Draw')
+} else if (playerSum > dealerSum) {
+    console.log(`dealer ${dealerSum} bust You Win`)
+} else {
+    console.log(`you bust dealer ${dealerSum} Win`)
+}
+
+
+
+   
